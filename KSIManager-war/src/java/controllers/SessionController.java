576dffc56bfb7518ffe0b742f265ca4fc5ebd421
Hristi <hrististoikov@gmail.com>
2017-09-21 13:00:55 +0200

    projet de base jusqu'à l'insertion du css

---
+++ KSIManager-war/src/java/controllers/SessionController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Hristi
+ */
+public class SessionController {
+
+    private String langue = "fr";
+    /**
+     * Creates a new instance of SessionController
+     */
+    public SessionController() {
+    }
+    
+    public String english() {
+        langue = "en";
+        return FacesContext.getCurrentInstance().getExternalContext().getRequestPathInfo() + "?faces-redirect=true";
+    }
+
+    public String french() {
+        langue = "fr";
+        return FacesContext.getCurrentInstance().getExternalContext().getRequestPathInfo() + "?faces-redirect=true";
+    }
+
+    public String getLangue() {
+        return langue;
+    }
+
+    public void setLangue(String langue) {
+        this.langue = langue;
+    }
+    
+    
+}
